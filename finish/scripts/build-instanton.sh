ab7751a649dcb6dd2b6782e6d3e81431e1dcecee
Thomas Watson <tjwatson@us.ibm.com>
2024-02-13 14:26:26 -0600

    Base InstantOn image on og image

--- finish/scripts/build-instanton.sh
+++ finish/scripts/build-instanton.sh
@@ -3,6 +3,7 @@
 sudo podman build \
    -f Dockerfile.instanton \
    -t getting-started \
+   --no-cache \
    --cap-add=CHECKPOINT_RESTORE \
    --cap-add=SYS_PTRACE\
    --cap-add=SETPCAP \
--- finish/scripts/build-og.sh
+++ finish/scripts/build-og.sh
@@ -3,6 +3,7 @@
 sudo podman build \
    -f Dockerfile \
    -t getting-started-og \
+   --no-cache \
    --cap-add=CHECKPOINT_RESTORE \
    --cap-add=SYS_PTRACE\
    --cap-add=SETPCAP \
